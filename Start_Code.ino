70c265fe1bc5e4a4ed0260bc0a4dff41acb4d7ad
minusmagis <33642118+minusmagis@users.noreply.github.com>
2020-06-15 10:32:12 +0200

    Update Start_Code.ino

--- Start_Code.ino
+++ Start_Code.ino
@@ -1,4 +1,5 @@
 void StartCode(){
+  Serial.println("XZ Stage Arduino");
   Serial.println("start");
   Serial.println("echo: XZ carriage 0.0.2");
   Serial.println("echo: Last Updated: 2020-06-03 | Author: minusmagis");
